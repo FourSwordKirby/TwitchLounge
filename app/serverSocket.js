5dfc9e6e5f1e430e1e8c5f391d5016488d9815c3
Angela Liu <angelamuliu@gmail.com>
2016-11-15 12:43:52 -0500

    Socket connects to namespaced lounge and initializes its own user object

--- app/serverSocket.js
+++ app/serverSocket.js
@@ -1,47 +1,34 @@
 
 var User = require('./models/user.js');
+var MongoDB = require('./models/mongo.js');
 
 exports.handleConnections = function(io, loungename) {
+// NAMESPACING - Creating lounge based on URL
+// Namespace creates a special lounge per streamer
+var nsp = io.of('/'+loungename);
+nsp.on('connection', function(socket){
+    console.log('someone connected to a namespaced lounge ' + loungename);
 
-    io.on('connection', function(socket){
-        console.log('a user connected');
+    var user;
 
-        // We imported our class User earlier, and can store information in it.
-        // var user = new User(socket, "test");
-
-        // When the user gets a socket event called 'chat message' it expects the request to also have a obj 'msg'
-        socket.on('chat message', function(msg){
-            // When this event happens, we then say we want to emit the event 'chat message' to EVERYONE connected!
-            io.emit('chat message', msg);
-            // console.log(user.username);
-
-        });
-
-        socket.on('disconnect', function(){
-            console.log('user disconnected');
-        });
-
-        // ------------------------------------------------------------
-        // HOST SOCKET EVENTS
-
-        socket.on('make lounge', function() {
-
+    socket.on('player: start', function(req) { // Authenticated user connected, try get user obj
+        MongoDB.getUser(req.twitch_id, req.access_token, function(row) {
+            if (row !== null) {
+                user = new User(row.twitch_id, row.twitch_username, row.twitch_avatar, row.twitch_bio, row.access_token);
+                socket.emit('player: add', user.jsonify());
+                socket.emit('test');
+            }
         })
+    })
 
+    socket.on('disconnect', function() {
+        console.log('user disconnected from lounge ' + loungename);
+    })
 
-
-        // ------------------------------------------------------------
-        // USER SOCKET EVENTS
-
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
     });
 
-    // NAMESPACING - Creating lounge based on URL
-    // Namespace creates a special lounge per streamer
-    var nsp = io.of('/'+loungename);
-    nsp.on('connection', function(socket){
-        console.log('someone connected to a namespaced lounge');
-    });
-
-
-}
+}); // Close namespace lounge socket
+} // Close serverSocket export
 
--- public/JS/clientSocket.js
+++ public/JS/clientSocket.js
@@ -1,18 +1,12 @@
 
-  var socket = io();
+var namespace = window.location.pathname;
+var socket = io(namespace);
 
-/* The clientside can send messages back to the server the same way, via socket.emit and more */
-
-  $('form').submit(function(){
-    debugger;
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
-  });
-  socket.on('chat message', function(msg){
+socket.on('chat message', function(msg){
     $('#messages').append($('<li>').text(msg));
-  });
-  socket.on('twitch message', function(msg){
+});
+
+socket.on('twitch message', function(msg){
     $('#messages').append(
         $('<li>').append(
             $('<span>').attr('style', msg[0]).text(msg[1]).append(
@@ -20,4 +14,42 @@
             )
         )
     )
-  });
+});
+
+
+
+// WIP below
+
+var twitch_id, access_token;
+var user, playerAvatar;
+
+$(document).ready(function() {
+    if (hasAuthenticated()) {
+        socket.emit('player: start', {access_token: access_token, twitch_id: twitch_id})
+    } else { lurk(); }
+
+    // TODO: Move later as we refactor this demo code into our own
+    $('form').submit(function(){
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+
+})
+
+socket.on('player: add', function(user) {
+    user = user;
+    playerAvatar = $("<div id=\'"+user.twitch_id+"\' class=\'player\' style=\'x:"+user.x+"; y:"+user.y+";\'></div>");
+    $("#players").append(playerAvatar);
+})
+
+
+function lurk() {} // TODO
+
+function hasAuthenticated() {
+    access_token = localStorage.getItem('lounge_token');
+    twitch_id = localStorage.getItem('twitch_id');
+    return access_token != null && twitch_id != null;
+}
+
+
